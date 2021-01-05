ac06f16e3185736609d8ecb3f14f4feca6c131c4
MinGwon Jang <alsrnjs0701@daum.net>
2021-01-05 11:31:05 +0900

    is making TodoPageCon

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -5,20 +5,27 @@
         <input type="text" @keyup.enter="insertTodo" :value="text"> <br>
         
         <todo-list
-            v-for="todoItem in todoList" 
+            v-for="todoItem in todoList"
             :key="todoItem.index" 
             :todoItem="todoItem"
             @deleteTodo="deleteTodo"
             @toggleCheckbox="toggleCheckbox"
         />
+
+        <todo-page-con 
+            :todoList="todoList"
+            :listRange="listRange"
+        />
     </div>
 </template>
 
 <script>
+import TodoPageCon from '../components/TodoPageCon.vue'
 import TodoList from '../components/TodoList.vue'
 export default {
     components: {
         TodoList,
+        TodoPageCon,
     },
 
     data() {
@@ -31,19 +38,35 @@ export default {
             text: '',
             count: 1,
             completedCount: 0,
+
+            firstList: 0,
+            lastList: 0,
+            listRange: 5,
+
+            pageCount: 0,
+            firstPage: 0,
+            nowPage: 0,
+            lastPage: 0,
+            startPage: 0,
+            endPage: 0,
+            pageRange1: 0,
+            pageRange2: 0,
         }
     },
 
     methods: {
         insertTodo(event) {
-            this.todoList.push({
-                text: event.target.value,
-                index: this.count,
-                checked: false,
-            });
-            this.count++;
-            console.log(this.todoList);
-            this.text ='';
+            for(let wow = 0; wow < 30 ; wow++){ //데이터 생산을 위해 만듬 끝나면 지우세요
+                this.todoList.push({
+                    text: event.target.value,
+                    index: this.count,
+                    checked: false,
+                });
+                this.count++;
+                console.log(this.todoList);
+                this.text ='';
+            }
+            
         },
         toggleCheckbox(index) {
             let index2 = this.todoList.findIndex(todo => todo.index == index);
